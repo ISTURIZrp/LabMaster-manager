7ea2f11fd4c54360f9167a6ac895508f35ef9935
ISTURIZrp <136546648+ISTURIZrp@users.noreply.github.com>
2025-04-29 18:42:00 -0600

    Actualizar login.js

--- Js/login.js
+++ Js/login.js
@@ -1,9 +1,15 @@
-function login() {
-  const user = document.getElementById('username').value;
-  const pass = document.getElementById('password').value;
-  if (user === "ISTURIZ" && pass === "EDELMIRA89") {
-    window.location.href = "index.html";
+document.getElementById('form-login').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const usuario = document.getElementById('usuario').value;
+  const clave = document.getElementById('clave').value;
+
+  const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+  const user = usuarios.find(u => u.usuario === usuario && u.clave === clave);
+
+  if (user) {
+    alert(`Bienvenido, ${user.usuario}`);
+    window.location.href = 'index.html';
   } else {
-    alert("Credenciales incorrectas");
+    alert('Credenciales incorrectas');
   }
-}
+});
